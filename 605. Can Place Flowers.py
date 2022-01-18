ab623f29459e2df9d5f44d25ed01f1ece6be0094
jack-goodson <jackmcph33@gmail.com>
2022-01-18 20:43:29 +1300

    init

---
+++ 605. Can Place Flowers.py
@@ -0,0 +1,32 @@
+from typing import List
+
+import self as self
+
+
+class Solution:
+
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowerbed_len: int
+        flowerbed_len = len(flowerbed)
+        flower_counter: int
+        flower_counter = 0
+        for pos in range(flowerbed_len):
+            if flowerbed_len == 1 and flowerbed[pos] == 0:
+                flower_counter +=1
+            elif pos == 0:
+                if flowerbed[pos] == 0 and flowerbed[pos+1] == 0:
+                    flowerbed[pos] = 1
+                    flower_counter += 1
+            elif pos != 0 and pos != flowerbed_len-1:
+                print(pos)
+                if (flowerbed[pos - 1] == 0) and (flowerbed[pos] == 0) and (flowerbed[pos + 1] == 0):
+                    flowerbed[pos] = 1
+                    flower_counter += 1
+            elif pos == flowerbed_len-1:
+                if flowerbed[pos] == 0 and flowerbed[pos-1] == 0:
+                    flowerbed[pos] = 1
+                    flower_counter += 1
+        return flower_counter >= n
+
+
+Solution.canPlaceFlowers(self, [1,0,0,0,1,0,0], 2)
